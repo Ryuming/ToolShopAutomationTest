03b8b20e6c6d80ab660a23eb07ba5a2fdb1e9c16
Ryuming <huynhtrongnghia10012000@gmail.com>
2025-03-17 10:29:32 +0700

    updated Excel TestCase Management

---
+++ src/test/java/ToolShopUI/component/TestCaseHomePage.java
@@ -0,0 +1,11 @@
+package ToolShopUI.component;
+
+public class TestCaseHomePage extends TestCase {
+    // Add test cases for the Home Page here
+
+    @Override
+    public void inputDataHandler() {
+
+        // Implement data input handling for the Home Page
+    }
+}
